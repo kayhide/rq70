827f704be078b4fdd2c99f72da2941a4eaad141f
kayhide <kayhide@gmail.com>
2016-09-28 00:58:31 +0900

    update 18 for correct answer.

--- questions/18/main.rb
+++ questions/18/main.rb
@@ -2,38 +2,37 @@ require 'benchmark'
 require 'pry'
 require 'set'
 
-$>.sync = true
-# module Q18
-#   module_function
+module Q18_
+  module_function
 
-#   class Piece < Struct.new :berry_count
-#   end
+  class Piece < Struct.new :berry_count
+  end
 
-#   class Whole < Struct.new :pieces
-#     def self.squared_nums
-#       @squared_nums ||= Set.new [1]
-#     end
+  class Whole < Struct.new :pieces
+    def self.squared_nums
+      @squared_nums ||= Set.new [1]
+    end
 
-#     def self.prepare_squared_nums n
-#       ((Math.sqrt(squared_nums.max).floor + 1)..Math.sqrt(n).ceil).each do |i|
-#         squared_nums << (i * i)
-#       end
-#     end
+    def self.prepare_squared_nums n
+      ((Math.sqrt(squared_nums.max).floor + 1)..Math.sqrt(n).ceil).each do |i|
+        squared_nums << (i * i)
+      end
+    end
 
-#     def self.create n
-#       prepare_squared_nums 2 * n
-#       (2..n).to_a.permutation.map do |xs|
-#         new [Piece.new(1), *xs.map { |x| Piece.new x }]
-#       end
-#     end
+    def self.create n
+      prepare_squared_nums 2 * n
+      (2..n).to_a.permutation.map do |xs|
+        new [Piece.new(1), *xs.map { |x| Piece.new x }]
+      end
+    end
 
-#     def valid?
-#       [*pieces, pieces.first].each_cons(2).all? do |pair|
-#         squared? pair.map(&:berry_count).inject(:+)
-#       end
-#     end
-#   end
-# end
+    def valid?
+      [*pieces, pieces.first].each_cons(2).all? do |pair|
+        squared? pair.map(&:berry_count).inject(:+)
+      end
+    end
+  end
+end
 
 module Q18
   module_function
@@ -48,45 +47,41 @@ module Q18
     end
   end
 
-  def squared? n
-    squared_nums.include? n
+  def squared? nums
+    squared_nums.include? nums.inject(:+)
   end
 
   def try_create n
     prepare_squared_nums 2 * n
-    pieces = (1..n).to_a
-    x = place [pieces.shift], pieces
+    place [1], (2..n).to_a
   end
 
   def place pieces, left
-    p pieces, left
-    if left.empty?
-      if squared?(pieces.first + pieces.last)
-        [pieces]
+    if pieces.count < 2 || squared?(pieces.last(2))
+      if left.empty?
+        if squared?([pieces.first, pieces.last])
+          [pieces]
+        else
+          []
+        end
       else
-        []
+        left.map { |p| place [*pieces, p], left - [p] }.inject(:+)
       end
     else
-      left.select { |p| squared?(pieces.last + p) }
-        .map { |p| place [*pieces, p], (left - [p]) }
+      []
     end
   end
 
   def integers
-    Enumerator.new do |y|
-      i = 32
-      loop do
-        y << i
-        i += 1
-      end
-    end
+    (1..Float::INFINITY).lazy
   end
 
   def run
-    x = integers.find do |i|
-      puts i
-      try_create i
-    end
+    res = integers
+          .map { |i| try_create i }
+          .drop_while(&:empty?)
+          .first
+    [res.first.count, res]
   end
 end
 
