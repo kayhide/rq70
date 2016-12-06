745b290c5bd38be811baa5b3c8d70f6ad0c25a27
kayhide <kayhide@gmail.com>
2016-12-06 21:25:30 +0900

    update 25 for exporting svg.

--- questions/25/main.rb
+++ questions/25/main.rb
@@ -26,6 +26,37 @@ module Q25
   def run
     xs = patterns.map { |p| [count_crossings(p), p] }
     x = xs.max_by(&:first)
+    x
+  end
+
+  class Exporter
+    def initialize options = {}
+      @width = options.fetch :width, 100
+      @height = options.fetch :height, 100
+      @interval = @height / (COUNT - 1)
+      @offset = options.fetch :offset, 5
+    end
+
+    def export pattern, file
+      open(file, 'w') { |io| io << <<~EOS }
+      <html>
+      <body>
+      <svg viewBox="#{-@offset} #{-@offset} #{@width + 2*@offset} #{@height + 2*@offset}">
+      <path stroke="black" stroke-width="1" fill="none" d="#{svg_path pattern}" />
+      </svg>
+      </body>
+      </html>
+      EOS
+    end
+
+    def svg_path pattern
+      pattern.map do |p|
+        [
+          "M #{@offset} #{p.first * @interval}",
+          "L #{@width} #{p.last * @interval}"
+        ]
+      end.flatten.join(" ")
+    end
   end
 end
 
